d6576f19a4ac8177b24e5f42a1dc48fb19c7c19b
Matt Scilipoti <matt@scilipoti.name>
2012-10-03 07:58:35 -0400

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HaydayHerald::Application.config.secret_token = '3fae5b0ec5930e6c650d1fb63f9b750d77c18257a105ac59ea6a9672f709e20bf7cfb61cd20d3ed3397cc8dea9b0c6422eb4f4d8e32c7d6ae556a3834ac0a83d'
