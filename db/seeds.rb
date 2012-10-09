17c907f91e78973566573c95796b92588ab572a4
Matt Scilipoti <matt@scilipoti.name>
2012-10-08 20:55:22 -0400

    seed crops

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+crops = %w(Wheat Corn Soybean Sugarcane Carrot Pumpkin Indigo)
+crops.each {|crop| Item.create!(:name => crop)}
