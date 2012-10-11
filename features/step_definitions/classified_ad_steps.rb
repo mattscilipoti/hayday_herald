e84ca5a4d0eaea805e89e65ffdfe3758ce84de38
Matt Scilipoti <matt@scilipoti.name>
2012-10-10 22:51:48 -0400

    feature: choose qty radio

--- features/step_definitions/classified_ad_steps.rb
+++ features/step_definitions/classified_ad_steps.rb
@@ -1,3 +1,9 @@
+When /select "(.*?)" as the Quantity$/ do |qty|
+  within '#quantity' do
+    choose qty
+  end
+end
+
 Then /should see these Classified Ads:$/ do |table|
   table.diff! tableish('#classified_ads tr', 'td,th')
 end
