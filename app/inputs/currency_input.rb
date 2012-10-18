2810b8b7fe4d29c4ca87488b36610ccd276ed111
Matt Scilipoti <matt@scilipoti.name>
2012-10-17 20:31:29 -0400

    simple_form: currency is number input, use min/max

--- app/inputs/currency_input.rb
+++ app/inputs/currency_input.rb
@@ -1,5 +1,5 @@
 class CurrencyInput < SimpleForm::Inputs::Base
   def input
-    "$ #{@builder.text_field(attribute_name, input_html_options)}".html_safe
+    "$ #{@builder.number_field(attribute_name, input_html_options)}".html_safe
   end
 end
