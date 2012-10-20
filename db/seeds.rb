444035395adecdb68f1bf9244134391085c79037
Matt Scilipoti <matt@scilipoti.name>
2012-10-19 21:31:49 -0400

    seeds: add clothes, tools, metal

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,8 +13,11 @@ if ENV['RESET'] =~ /true/i
 end
 
 puts "Seeding db..."
+clothes = %w(BlueSweater BlueWoolyHat BlueSweater)
 crops = %w(Wheat Corn Soybean Sugarcane Carrot Pumpkin Indigo ChiliPepper Raspberry Apple)
 goods = %w(Bread Cookie CornBread Cream Butter Cheese Popcorn ButterPopcorn ChiliPopcorn Hamburger BaconAndEggs Pancake CarrotPie PumkpinPie BaconPie BrownSugar WhiteSugar Syrup Cheesecake CarrotCake ButterCake)
 animal_goods = %w(Eggs Milk Bacon Wool)
 animal_food = %w(ChickenFood CowFood PigFood SheepFood GoatFood)
-(crops | goods | animal_goods | animal_food).each {|crop| Item.find_or_create_by_name(:name => crop)}
+tools = %w(Axe Bolt DuctTape Dynamite Plank Saw Screw Shovel TNTBarrel WoodPanel)
+metal = %w(SilverBar SilverOre)
+(clothes | crops | goods | animal_goods | animal_food | metal | tools).each {|crop| Item.find_or_create_by_name(:name => crop)}
